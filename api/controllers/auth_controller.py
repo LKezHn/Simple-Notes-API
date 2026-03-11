2f46934fee204d89dc60124c75ae240fe0630dd6
Luis Martinez <mluise2406@gmail.com>
2026-03-10 19:31:16 -0600

    Initial register controller logic added

--- api/controllers/auth_controller.py
+++ api/controllers/auth_controller.py
@@ -3,8 +3,13 @@ from pymongo.errors import DuplicateKeyError
 
 def register_user(username, email, password):
     
-    mongo.db.users.insert_one({
-        "username": username,
-        "email": email,
-        "password": password
-    })
+    try:
+        mongo.db.users.insert_one({
+            "username": username,
+            "email": email,
+            "password": password
+        })
+
+        return "User registered", None
+    except DuplicateKeyError:
+        return None, "username or email already exists"
--- api/routes/auth_routes.py
+++ api/routes/auth_routes.py
@@ -1,7 +1,9 @@
+from flask import request
 from flask_restx import Resource
 
 from api.namespaces.namespaces import auth_ns
 from api.models.user_model import auth_models
+from api.controllers.auth_controller import register_user
 
 login_model, register_model = auth_models(auth_ns)
 
@@ -24,11 +26,6 @@ class Login(Resource):
 @auth_ns.route('/register')
 class Register(Resource):
     @auth_ns.doc(description="Register endpoint",
-             params={
-                 "username": "User name",
-                 "email": "User's email",
-                 "password": "User's password"
-             },
              responses={
                  200: 'User logged',
                  400: "Incorrect user or password"
@@ -36,5 +33,13 @@ class Register(Resource):
         )
     @auth_ns.expect(register_model)
     def post(self):
-            return {"status": 200, "token": "JSON Web Token"}
+        username = request.json["username"]
+        email = request.json["email"]
+        password = request.json["password"]
+
+        message, error = register_user(username, email, password)
     
+        if error != None:
+            return { "status": 400, "message": error}
+
+        return { "status": 200, "message": message}

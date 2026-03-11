d470778c6f2591f4baeef83893738aae0a209bf3
Luis Martinez <mluise2406@gmail.com>
2026-03-10 19:38:59 -0600

    encrypt password on register added

--- api/controllers/auth_controller.py
+++ api/controllers/auth_controller.py
@@ -1,15 +1,20 @@
+import bcrypt
 from db import mongo
 from pymongo.errors import DuplicateKeyError
 
 def register_user(username, email, password):
     
+    # Hashing password for security
+    encrypted_pass = bcrypt.hashpw( password.encode("utf-8"),bcrypt.gensalt()).decode("utf-8")
+
     try:
         mongo.db.users.insert_one({
             "username": username,
             "email": email,
-            "password": password
+            "password": encrypted_pass
         })
 
         return "User registered", None
+    
     except DuplicateKeyError:
         return None, "username or email already exists"

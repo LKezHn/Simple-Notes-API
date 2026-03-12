2f4c5a398aff886fdba7e952810d5e9d3999dc39
Luis Martinez <mluise2406@gmail.com>
2026-03-11 19:37:21 -0600

    Todos CRUD was added

--- api/controllers/todo_controller.py
+++ api/controllers/todo_controller.py
@@ -1,15 +1,60 @@
+from bson import ObjectId
+from bson.json_util import dumps
+
+from pymongo import ReturnDocument
+
 from db import mongo 
 
-def get_todos():
-    todos = mongo.db.todos.find()
+def get_todos(user_id):
+    todos = mongo.db.todos.find({"user_id": user_id})
     result = []
 
     for todo in todos:
         result.append({
             "id": str(todo["_id"]),
-            "title": todo.title,
-            "description": todo.description,
-            "status": todo.status
+            "title": todo["title"],
+            "description": todo["description"],
+            "status": todo["status"]
         })
 
     return result
+
+def add_todo(title, description, status, user_id):
+    
+    todo = mongo.db.todos.insert_one({
+        "user_id": user_id,
+        "title": title,
+        "description": description,
+        "status": status
+    })
+    
+    return "%s"%todo.inserted_id
+
+def get_todo(todo_id, user_id):
+    todo = mongo.db.todos.find_one({"_id": ObjectId(todo_id), "user_id": user_id})
+    if not todo:
+        return None, "Not found"
+    
+    todo["_id"] = str(todo["_id"])
+
+    return todo, None
+
+def update_todo(todo_id, user_id, title, description, status):
+    todo = mongo.db.todos.find_one_and_update({"_id": ObjectId(todo_id), "user_id": user_id}, { "$set": { "title": title, "description": description, "status": status}}, return_document=ReturnDocument.AFTER)
+
+    if not todo:
+         return None, "Not found"
+    
+    todo["_id"] = str(todo["_id"])
+
+    return todo, None
+
+def delete_todo(todo_id, user_id):
+    todo = mongo.db.todos.find_one_and_delete({"_id": ObjectId(todo_id), "user_id": user_id})
+
+    if not todo:
+        return None, "Not found"
+    
+    todo["_id"] = str(todo["_id"])
+
+    return "Todo was deleted", None
--- api/routes/todo_routes.py
+++ api/routes/todo_routes.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restx import Resource
 
 from flask_jwt_extended import jwt_required, get_jwt_identity
@@ -5,10 +6,9 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from api.namespaces.namespaces import todo_ns
 from api.models.todo_model import todo_model
 
-from api.controllers.todo_controller import get_todos
+from api.controllers.todo_controller import get_todos, add_todo, get_todo, update_todo, delete_todo
 
 todo_model = todo_model(todo_ns)
-
 
 @todo_ns.route('/')
 @todo_ns.doc(security="Bearer")
@@ -17,15 +17,19 @@ class Todos(Resource):
     @jwt_required()
     def get(self):
         user_id = get_jwt_identity()
-        todos = get_todos()
+        todos = get_todos(user_id)
 
         return {"status": 200, "todos": todos}
 
-    @todo_ns.doc(description="Add a new todo", responses={ 200: 'User logged', 400: "Incorrect user or password"})
+    @todo_ns.doc(description="Add a new todo", responses={ 200: 'User logged', 400: "An error was ocurred"})
     @todo_ns.expect(todo_model)
     @jwt_required()
     def post(self):
-        pass
+        user_id = get_jwt_identity()
+        title, description, status = request.json.values()
+        todo = add_todo(title, description, status, user_id)
+
+        return { "status": 200, "todo": todo }
 
 @todo_ns.route('/<string:todo_id>')
 @todo_ns.doc(security="Bearer")
@@ -33,14 +37,36 @@ class Todo(Resource):
     @todo_ns.doc(description="Get one todo", responses={ 200: 'Todo exists', 400: "Todo doesn't exists" })
     @jwt_required()
     def get(self, todo_id):
-        return { "Info": todo_id}
+        user_id = get_jwt_identity()
+
+        todo, error = get_todo(todo_id, user_id)
+        if error != None:
+            return { "status": 400, "message": error }
+        
+        return { "status": 200, "todo": todo}
 
     @todo_ns.doc(description="Update a todo", responses={ 200: 'Todo updated', 400: "Todo doesn't updated or not exists" })
+    @todo_ns.expect(todo_model)
     @jwt_required()
     def put(self, todo_id):
-        return { "Info": todo_id}
+        user_id = get_jwt_identity()
+
+        title, description, status = request.json.values()
+
+        todo, error = update_todo(todo_id, user_id, title, description, status)
+        
+        if error != None:
+            return { "status": 400, "message": error }
+        
+        return { "status": 200, "message": "todo updated","todo": todo}
     
     @todo_ns.doc(description="Delete a todo", responses={ 200: 'Todo was deleted', 400: "Todo doesn't was deleted" })
     @jwt_required()
     def delete(self, todo_id):
-        return { "Info": todo_id}
+        user_id = get_jwt_identity()
+
+        message, error = delete_todo(todo_id, user_id)
+        if error != None:
+            return { "status": 400, "message": error }
+        
+        return { "status": 200, "message": message}
